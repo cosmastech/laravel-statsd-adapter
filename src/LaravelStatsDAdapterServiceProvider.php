2c17a4eef5a7bdc67ce71b1321e22d70c2e86eba
Luke Kuzmish <lakuzmish@gmail.com>
2024-07-04 22:42:45 -0400

    stub service provider

---
+++ src/LaravelStatsDAdapterServiceProvider.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Cosmastech\LaravelStatsDAdapter;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelStatsDAdapterServiceProvider extends ServiceProvider
+{
+    // @todo
+}
