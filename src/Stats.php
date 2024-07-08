b8a0bd0a6da26c922cb8cb49ce0a89276e37d8c8
Luke Kuzmish <lakuzmish@gmail.com>
2024-07-08 10:11:09 -0400

    update facade docblock

--- src/Stats.php
+++ src/Stats.php
@@ -8,6 +8,8 @@ use Illuminate\Support\Facades\Facade;
  * @method static void getDefaultInstance()
  * @method static void setDefaultInstance(string $name)
  * @method static array|null getInstanceConfig(string $name)
+ * @method static void setDefaultTags(array $tags)
+ * @method static array getDefaultTags()
  * @method static mixed instance(string|null $name = null)
  * @method static \Cosmastech\LaravelStatsDAdapter\AdapterManager forgetInstance(array|string|null $name = null)
  * @method static void purge(string|null $name = null)
@@ -21,8 +23,6 @@ use Illuminate\Support\Facades\Facade;
  * @method static void decrement(array|string $stats, float $sampleRate = 1, array $tags = [], int $value = 1)
  * @method static void updateStats(array|string $stats, int $delta = 1, void $sampleRate = 1, void $tags = null)
  * @method static mixed getClient()
- * @method static void setDefaultTags(array $defaultTags = [])
- * @method static array getDefaultTags()
  *
  * @see \Cosmastech\LaravelStatsDAdapter\AdapterManager
  */
