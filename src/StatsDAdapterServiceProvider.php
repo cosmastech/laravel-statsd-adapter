8ec4b0b2d4973bf758d275a13401e41e3b9f1df7
Luke Kuzmish <lakuzmish@gmail.com>
2024-07-09 15:24:04 -0400

    offer publishing

--- src/StatsDAdapterServiceProvider.php
+++ src/StatsDAdapterServiceProvider.php
@@ -9,6 +9,11 @@ use Illuminate\Support\ServiceProvider;
 
 class StatsDAdapterServiceProvider extends ServiceProvider implements DeferrableProvider
 {
+    public function boot(): void
+    {
+        $this->offerPublishing();
+    }
+
     public function register(): void
     {
         $this->app->singleton(
@@ -28,5 +33,16 @@ class StatsDAdapterServiceProvider extends ServiceProvider implements Deferrable
     public function provides(): array
     {
         return [AdapterManager::class, StatsDClientAdapter::class];
+    }
+
+    protected function offerPublishing(): void
+    {
+        if (! $this->app->runningInConsole()) {
+            return;
+        }
+
+        $this->publishes([
+            __DIR__.'/../config/statsd-adapter.php' => config_path('statsd-adapter.php.php'),
+        ], 'statsd-config');
     }
 }
